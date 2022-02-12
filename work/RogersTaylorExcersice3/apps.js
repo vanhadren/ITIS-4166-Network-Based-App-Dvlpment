516edc88dcc36ad55af77efb7c4a532b8b5338c6
Vanhadren <troger43@uncc.edu>
2022-02-11 23:08:50 -0500

    .

--- work/RogersTaylorExcersice3/apps.js
+++ work/RogersTaylorExcersice3/apps.js
@@ -1,31 +1,45 @@
 const express = require('express');
+
 const app = express();
 let port = 3000;
 let host = 'localhost'
 
-let students = [{id: 1 , name: 'alice', major: 'itcs'}, 
-                {id: 2 , name: 'bob', major: 'bio'},
-                {id: 3 , name: 'Charlie', major: 'phy'}];
+let students = [
+    {id: 1 , name: 'alice', major: 'itcs'}, 
+    {id: 2 , name: 'bob', major: 'bio'},
+    {id: 3 , name: 'Charlie', major: 'phy'}
+];
 
+app.use(express.static('public'));
+
+/** needs to be on top or work be ran */
 app.use((req, res, next) => {
-    console.log(res.method);
-    console.log(res.url);
+    console.log(req.method);
+    console.log(req.url);
+    next();
+
+});
+/*
+app.use((req, res, next) => {
+    console.log('this is the second middleware function');
+    next();
+    //res.send('second middleware');
+   });
+*/
+app.get('/', (req, res) => {
+    //res.statusCode = 200;
+    //res.end('Home Page');
+    //res.send('Home Page');  /** short-hand version of above code is auto set to 302(found) */ 
+    //console.log(__dirname);
+
+    console.log(req.url);
+    console.log(req.query);
+    res.sendFile('./public/views/index.html', { root:__dirname });
+    
 });
 
 app.get('/students', (req, res) => {
     res.json(students);
-});
-
-app.get('/', (req, res) => {
-    //res.statusCode = 200;
-    //res.end('Home Page');
-/** short-hand version of above */
-    //res.send('Home Page');    
-    //console.log(__dirname);
-    console.log(req.url);
-    console.log(req.query);
-    res.sendFile('./views/index.html', { root:__dirname });
-    
 });
 
 //send students with particular id back to the client
@@ -62,6 +76,10 @@ app.get('/contact', (req, res) => {
 app.get('/contact-us', (req, res) => {
     res.redirect(301,'/contact');
 });
+
+app.use((req, res, next) => {
+    res.status(404).send('page cannot be found');
+})
 
 app.listen(port, host, () => { 
         console.log("The server is runing at port: " ,port);
