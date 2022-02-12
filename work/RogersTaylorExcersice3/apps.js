e547646b2cf1146d447bff0122ffb101bfa3a308
Vanhadren <troger43@uncc.edu>
2022-02-11 21:02:22 -0500

    .

--- work/RogersTaylorExcersice3/apps.js
+++ work/RogersTaylorExcersice3/apps.js
@@ -3,6 +3,30 @@ const app = express();
 let port = 3000;
 let host = 'localhost'
 
+
+app.get('/', (req, res) => {
+    //res.statusCode = 200;
+    //res.end('Home Page');
+/** short-hand version of above */
+    //res.send('Home Page');    
+    console.log(__dirname);
+    res.sendFile('.views/index.html', { root:__dirname });
+    
+    
+});
+app.get('/about', (req, res) => {
+    //res.statusCode = 200;
+    //res.end('Home Page');
+/** short-hand version of above */
+    res.send('About Page');
+});
+app.get('/contact', (req, res) => {
+    //res.statusCode = 200;
+    //res.end('Home Page');
+/** short-hand version of above */
+    res.send('Contact Page');
+});
+
 app.listen(port, host, () => { 
         console.log("The server is runing at port: " ,port);
     });
